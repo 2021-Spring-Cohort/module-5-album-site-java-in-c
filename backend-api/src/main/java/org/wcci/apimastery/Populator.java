f9acbb16c036c44226bd7b6cb95a1fcf9a5d4618
Bishnu <bdahal17@gmail.com>
2021-04-01 11:58:59 -0400

    update

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -26,6 +26,7 @@ public class Populator implements CommandLineRunner {
         // Albums ----------------//
         // Albums ----------------//
         // Albums ----------------//
+        Album NineteenEightyNine  = new Album ("1989","");
         Album EmergencyTsunami = new Album("Emergency Tsunami", "/src/images/metallica.jpg");
         Album AndJusticeForAll = new Album("...And Justice For All", "/src/images/metallica.jpg");
         // Artists ----------------//
@@ -33,6 +34,7 @@ public class Populator implements CommandLineRunner {
         // Artists ----------------//
         Artist Metallica = new Artist("Metallica");
         Artist Nav = new Artist("Nav");
+        Artist TylerSwift = new Artist("Tyler Swift");
         // Songs ----------------//
         // Songs ----------------//
         // Songs ----------------//
@@ -48,30 +50,31 @@ public class Populator implements CommandLineRunner {
         Song frayedEnds = new Song("The Frayed Ends of Sanity", "sample", AndJusticeForAll);
         Song toLiveIsToDie = new Song("To Live Is to Die", "sample", AndJusticeForAll);
         Song dyersEve = new Song("Dyers Eve", "sample", AndJusticeForAll);
-      
-       Album NinteenEightyNine  = new Album ("1989","");
-        Song WelcomeToNewYork = new Song("WelcomeToNewYork", "sample",NinteenEightyNine );
-        Song BlankSpace = new Song("BlankSpace", "sample", NinteenEightyNine);
-        Song Style = new Song("Style", "sample", NinteenEightyNine);
-        Song OutOfTheWoods = new Song("OutOfTheWoods", "sample", NinteenEightyNine);
-        Song AllYouHadToDoWasStay = new Song("AllYouHadToDoWasStay", "sample", NinteenEightyNine);
-        Song ShakeItOff = new Song("ShakeItOff", "sample", NinteenEightyNine);
-        Song IWishYouWould = new Song("IWishYouWould", "sample", NinteenEightyNine);
-        Song BadBlood = new Song("BadBlood", "sample", NinteenEightyNine);
-        Song WildestDreams = new Song("WildestDreams", "sample", NinteenEightyNine);
-        Song HowYouGetTheGirl = new Song("HowYouGetTheGirl", "sample", NinteenEightyNine);
-        Song ThisLove = new Song("ThisLove","sample", NinteenEightyNine);
-        Song IknowPlaces = new Song("IknowPlaces", "sample", NinteenEightyNine);
-        Song Clean = new Song("Clean", "sample", NinteenEightyNine);
+        Song WelcomeToNewYork = new Song("WelcomeToNewYork", "sample",NineteenEightyNine );
+        Song BlankSpace = new Song("BlankSpace", "sample", NineteenEightyNine);
+        Song Style = new Song("Style", "sample", NineteenEightyNine);
+        Song OutOfTheWoods = new Song("OutOfTheWoods", "sample", NineteenEightyNine);
+        Song AllYouHadToDoWasStay = new Song("AllYouHadToDoWasStay", "sample", NineteenEightyNine);
+        Song ShakeItOff = new Song("ShakeItOff", "sample", NineteenEightyNine);
+        Song IWishYouWould = new Song("IWishYouWould", "sample", NineteenEightyNine);
+        Song BadBlood = new Song("BadBlood", "sample", NineteenEightyNine);
+        Song WildestDreams = new Song("WildestDreams", "sample", NineteenEightyNine);
+        Song HowYouGetTheGirl = new Song("HowYouGetTheGirl", "sample", NineteenEightyNine);
+        Song ThisLove = new Song("ThisLove","sample", NineteenEightyNine);
+        Song IknowPlaces = new Song("IknowPlaces", "sample", NineteenEightyNine);
+        Song Clean = new Song("Clean", "sample", NineteenEightyNine);
+
+        // adding Repos ----------------//
+        // adding Repos ----------------//
+        // adding Repos ----------------//
+        albumRepository.save(AndJusticeForAll);
+        artistRepository.save(Metallica);
+        albumRepository.save(NineteenEightyNine);
+        artistRepository.save(TylerSwift);
+
 
         Metallica.addAlbum(AndJusticeForAll);
-        artistRepository.save(Metallica);
-        Artist TylerSwift = new Artist("Tyler Swift");
-        albumRepository.save(NinteenEightyNine);
-        TylerSwift.addAlbum(NinteenEightyNine);
-        
-
-       
+        TylerSwift.addAlbum(NineteenEightyNine);
 
         WelcomeToNewYork.addArtist(TylerSwift);
         BlankSpace.addArtist(TylerSwift);
@@ -86,23 +89,6 @@ public class Populator implements CommandLineRunner {
         ThisLove.addArtist(TylerSwift);
         IknowPlaces.addArtist(TylerSwift);
         Clean.addArtist(TylerSwift);
-
-
-        songRepository.save(WelcomeToNewYork);
-        songRepository.save(BlankSpace);
-        songRepository.save(Style);
-        songRepository.save(OutOfTheWoods);
-        songRepository.save(AllYouHadToDoWasStay);
-        songRepository.save(ShakeItOff);
-        songRepository.save(IWishYouWould);
-        songRepository.save(BadBlood);
-        songRepository.save(WildestDreams);
-        songRepository.save(HowYouGetTheGirl);
-        songRepository.save(ThisLove);
-        songRepository.save(IknowPlaces);
-        songRepository.save(Clean);
-
-        
         andJusticeForAll.addArtist(Metallica);
         blackened.addArtist(Metallica);
         eyeOfTheBeholder.addArtist(Metallica);
@@ -122,5 +108,18 @@ public class Populator implements CommandLineRunner {
         songRepository.save(frayedEnds);
         songRepository.save(toLiveIsToDie);
         songRepository.save(dyersEve);
+        songRepository.save(WelcomeToNewYork);
+        songRepository.save(BlankSpace);
+        songRepository.save(Style);
+        songRepository.save(OutOfTheWoods);
+        songRepository.save(AllYouHadToDoWasStay);
+        songRepository.save(ShakeItOff);
+        songRepository.save(IWishYouWould);
+        songRepository.save(BadBlood);
+        songRepository.save(WildestDreams);
+        songRepository.save(HowYouGetTheGirl);
+        songRepository.save(ThisLove);
+        songRepository.save(IknowPlaces);
+        songRepository.save(Clean);
     }
 }
