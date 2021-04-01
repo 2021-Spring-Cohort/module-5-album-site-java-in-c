b1bc90a6f3518f8be946cc9eb1daa515bd55386f
miguel garcia <sinxash8@gmail.com>
2021-04-01 12:58:18 -0400

    miguls album

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -63,7 +63,51 @@ public class Populator implements CommandLineRunner {
         songRepository.save(dyersEve);
 
 
-
-
     }
+
+    @Override
+    public void run(String... args) throws Exception{
+        Album igor = new Album("Igor", "/src/images/Igor.jpg");
+        Artist tylerTheCreator = new Artist("Tyler, The Creator");
+        Song igorsTheme = new Song("Igor's Theme", "sample", igor);
+        Song earfquake = new Song("Earfquake", "sample", igor);
+        Song iThink = new Song("I Think", "sample", igor);
+        Song runningOutOfTime = new Song("Running Out Of Time", "sample", album1);
+        Song newMagicWand = new Song("New Magic Wand", "sample", igor);
+        Song ABoyIsAGun = new Song("A Boy Is A Gun", "sample", igor);
+        Song puppet = new Song("Puppet", "sample", igor);
+        Song whatsGood = new Song("What's Good", "sample", igor);
+        Song goneGoneThankYou = new Song("Gone, Gone/Thank You", "sample", igor);
+        Song iDontLoveYouAnymore = new Song("I Don't Love You Anymore", "sample", igor);
+        Song areWeStillFriends = new Song("Are We Still Friends", "sample", igor);
+
+        albumRepository.save(igor);
+
+        artist1.addAlbum(igor);
+        artistRepository.save(tylerTheCreator);
+
+        igorsTheme.addArtist(igor);
+        earfquake.addArtist(igor);
+        iThink.addArtist(igor);
+        runningOutOfTime.addArtist(igor);
+        newMagicWand.addArtist(igor);
+        ABoyIsAGun.addArtist(igor);
+        puppet.addArtist(igor);
+        whatsGood.addArtist(igor);
+        goneGoneThankYou.addArtist(igor);
+        iDontLoveYouAnymore.addArtist(igor);
+        areWeStillFriends.addArtist(igor);
+        songRepository.save(igorsTheme);
+        songRepository.save(earfquake);
+        songRepository.save(iThink);
+        songRepository.save(runningOutOfTime);
+        songRepository.save(newMagicWand);
+        songRepository.save(ABoyIsAGun);
+        songRepository.save(puppet);
+        songRepository.save(whatsGood);
+        songRepository.save(goneGoneThankYou);
+        songRepository.save(iDontLoveYouAnymore);
+        songRepository.save(areWeStillFriends);
+    }
+
 }
